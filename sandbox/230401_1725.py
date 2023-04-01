36163e191b2bbfa95039a4f94bbc48df9437a7cc
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-01 22:39:35 +0900

    intoimg

--- sandbox/230401_1725.py
+++ sandbox/230401_1725.py
@@ -1,15 +1,25 @@
+from io import BytesIO
+from PIL import Image as ImageP
 import qrcode
+
 import ui
+
+
+def img2ui_img(img_p: ImageP):
+  kind = img_p.kind
+  with BytesIO() as bIO:
+    img_p.save(bIO, kind)
+    return ui.Image.from_data(bIO.getvalue())
 
 
 class View(ui.View):
   def __init__(self, data=None, *args, **kwargs):
     self.bg_color = 1
-    img = qrcode.make(data)
+    self.img = qrcode.make(data)
     self.iv = ui.ImageView()
-    self.iv.image = ui.Image.from_data(img)
+    self.iv.image = img2ui_img(self.img)
+    #self.iv.image = ui.Image.from_data(img)
     self.add_subview(self.iv)
-    
 
 
 if __name__ == '__main__':
