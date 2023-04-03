8b38665ca33aa0acad17c6efded42654e9578394
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-03 11:33:43 +0900

    layout

--- sandbox/230402_1600.py
+++ sandbox/230402_1600.py
@@ -11,27 +11,48 @@ def img2ui_img(img_p: ImageP):
     img_p.save(bIO, kind)
     return ui.Image.from_data(bIO.getvalue())
 
+
 def create_btn(icon):
   btn_icon = ui.Image.named(icon)
   btn = ui.Button(image=btn_icon)
   return btn
 
+
 class InPutViews(ui.View):
   def __init__(self, parent, *args, **kwargs):
     self.parent = parent
     self.bg_color = 'green'
-    
+
     self.text_field = ui.TextField()
-    self.text_field.flex = 'WH'
-    
+    self.text_field.height = 48
+    self.text_field.flex = 'W'
+
     self.btn = create_btn('iob:ios7_refresh_outline_256')
-    
-    
-    self.add_subview(self.btn)
+    self.btn = self.set_btn_size_radius(self.btn)
+    self.btn.bg_color = 'red'
+
     self.add_subview(self.text_field)
-    
+    self.add_subview(self.btn)
+
+  def set_btn_size_radius(self, _btn):
+    btn_size = min(self.width, self.height) / 2
+    _btn.width = _btn.height = btn_size
+    _btn.corner_radius = btn_size / 2.0
+    return _btn
+
   def layout(self):
-    print(self.parent.frame)
+    _, _, w, h = self.parent.frame
+    margin = w / 24
+    self.x = margin
+    self.width = w - (margin * 2.0)
+
+    self.btn = self.set_btn_size_radius(self.btn)
+    self.btn.y = margin
+    self.btn.x = (self.width / 2.0) - (self.btn.width / 2.0)
+
+    self.text_field.y = self.btn.y + self.btn.height + margin
+
+    self.height = self.text_field.height + self.text_field.y + margin
 
 
 class View(ui.View):
@@ -44,17 +65,12 @@ class View(ui.View):
 
     self.input_view = InPutViews(self)
     self.add_subview(self.input_view)
-
-    
-    #self.add_subview(self.img_view)
+    self.add_subview(self.img_view)
 
   def layout(self):
-    pass
-
-  def create_btn(self, icon):
-    btn_icon = ui.Image.named(icon)
-    btn = ui.Button(image=btn_icon)
-    return btn
+    #self.img_view.y = self.input_view.height + self.y
+    self.img_view.x = (self.width / 2.0) - (self.img_view.width / 2.0)
+    self.input_view.y = self.y + self.img_view.height
 
 
 if __name__ == '__main__':
