df237e54ac6a7daed94d3a332dab3ddeddfec040
pome-ta <53405097+pome-ta@users.noreply.github.com>
2023-04-03 22:42:17 +0900

    copy modules

---
+++ qrcode/constants.py
@@ -0,0 +1,6 @@
+#import 'pythonista'
+# QR error correct levels
+ERROR_CORRECT_L = 1
+ERROR_CORRECT_M = 0
+ERROR_CORRECT_Q = 3
+ERROR_CORRECT_H = 2
---
+++ sandbox/230403_1310.py
@@ -0,0 +1,15 @@
+# モジュールコピー
+
+from pathlib import Path
+import shutil
+
+import qrcode
+
+
+modules_file_path = qrcode.__file__
+modules_name = qrcode.__name__
+copy_path = Path(modules_file_path).parent
+
+to_path = Path(f'../{modules_name}').resolve()
+
+shutil.copytree(copy_path, to_path)
